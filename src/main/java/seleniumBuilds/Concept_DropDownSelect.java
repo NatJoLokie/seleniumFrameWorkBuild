54193bc614167fa2b79920c4db7a5e52372213eb
LokiEYohan <lok.war74@gmail.com>
2024-06-26 00:27:25 +0530

    Select Dropdown methods concept

---
+++ src/main/java/seleniumBuilds/Concept_DropDownSelect.java
@@ -0,0 +1,53 @@
+package seleniumBuilds;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Concept_DropDownSelect {
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        driver = new ChromeDriver();
+
+        driver.get("https://www.orangehrm.com/30-day-free-trial/");
+        driver.manage().window().maximize();
+
+        WebElement countries = driver.findElement(By.id("Form_getForm_Country"));
+
+        Select select = new Select(countries);
+
+        select.selectByIndex(5);
+
+        select.selectByVisibleText("India");
+
+        select.selectByValue("Australia");
+    }
+
+    public static WebElement getElement(By locator) {
+        return driver.findElement(locator);
+    }
+
+    public static void doSelectByIndex(By locator, int index) {
+        Select select = new Select(getElement(locator));
+        select.selectByIndex(index);
+    }
+
+    public static void doSelectByVisibleText(By locator, String visibleText) {
+        if (visibleText.length() == 0 || visibleText == null) {
+            throw new ExceptionMyElement("Visible Text cannot be null or blank");
+        }
+        Select select = new Select(getElement(locator));
+        select.selectByVisibleText(visibleText);
+    }
+
+    public static void doSelectByValue(By locator, String value) {
+        if (value.length() == 0 || value == null) {
+            throw new ExceptionMyElement("Visible Text cannot be null or blank");
+        }
+        Select select = new Select(getElement(locator));
+        select.selectByValue(value);
+    }
+}
