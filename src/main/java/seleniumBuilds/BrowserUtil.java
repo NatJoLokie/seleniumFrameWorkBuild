569551f5b8cb773cb0bd65025cc9ee2572bc45eb
LokiEYohan <lok.war74@gmail.com>
2024-07-04 00:13:13 +0530

    Updated documentation comments

--- src/main/java/seleniumBuilds/BrowserUtil.java
+++ src/main/java/seleniumBuilds/BrowserUtil.java
@@ -2,6 +2,7 @@ package seleniumBuilds;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
